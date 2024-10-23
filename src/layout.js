1e85ca9b7340c6c0ec3c673ae5a2aae8315ffd18
lianecc <liane.coupat@gmail.com>
2024-10-23 13:49:43 +0200

    adding images

--- src/layout.js
+++ src/layout.js
@@ -1,13 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './styles/layout.scss';
+import logo from "./images/logo-kasa.png"
 
 const Layout = ({ children }) => {
     return (
         <div className="layout">
             <header>
+                <img src={logo} alt="Logo" />
                 <nav>
-                    <Link to ="/"> Accueil </Link>
-                    <Link to ="/apropos">A propos</Link>
+                    <ul>
+                        <li><Link to="/">Accueil</Link></li>
+                    <li><Link to="/apropos">À propos</Link></li>
+                    </ul>
                 </nav>
             </header>
             <main>{children}</main>
