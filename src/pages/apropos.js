abfc6d2a7dba987787fe977d3ec83a1a4093790e
lianecc <liane.coupat@gmail.com>
2024-10-23 12:39:40 +0200

    adding layout.js, modification src/pages

---
+++ src/pages/apropos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const APropos = () => {
+    return (
+        <div>
+            <h1>À Propos</h1>
+            <p>fiabilité, respect, service, sécurité</p>
+        </div>
+    );
+};
+
+export default APropos;
---
+++ src/pages/fichelogement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FicheLogement = () => {
+    return (
+        <div>
+            <h1>fiches logements</h1>
+        </div>
+    );
+};
+
+export default FicheLogement;
