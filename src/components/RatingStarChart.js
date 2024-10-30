01fd6c7367d4db0c2f7d885158b9132293767b1f
lianecc <liane.coupat@gmail.com>
2024-10-30 21:59:29 +0100

    updating RatingStarCharts and correcting indentations

--- src/components/RatingStarChart.js
+++ src/components/RatingStarChart.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './RatingStarChart.scss';
+import starFilled from '../images/star-red.png';
+import starEmpty from '../images/star-grey.png';
 
 const RatingStarChart = ({ rating }) => {
     return (
         <div className="rating">
             {Array.from({ length: 5 }, (_, i) => (
-                <span key={i} className={`star ${i < rating ? "filled" : ""}`}>★</span>
+                <img
+                    key={i}
+                    src={i < rating ? starFilled : starEmpty} 
+                    alt={i < rating ? "Étoile remplie" : "Étoile vide"}
+                    className="star"
+                />
             ))}
         </div>
     );
